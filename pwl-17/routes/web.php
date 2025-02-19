05a28ba3f32d024cf8ed8965bbcf87b90d357f72
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2025-02-19 08:21:22 +0700

    tambah parameter opsional

--- pwl-17/routes/web.php
+++ pwl-17/routes/web.php
@@ -29,7 +29,7 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
 
-Route::get('/articles/{id}', function ($id) {
+Route::get('/articles/{id?}', function ($id = 17) {
     return "Halaman Artikel dengan ID $id";
 });
 
