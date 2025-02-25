829227482154c3ee11d0441084dc6071c71c4ff7
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2025-02-25 18:51:03 +0700

    Praktikum 6 selesai

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -11,20 +11,31 @@ class UserController extends Controller
     public function index()
     {
         // Tambah data user dengan Eloquent Model
-        $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4
-        ];
+        //$data = [
+        //    'username' => 'customer-1',
+        //    'nama' => 'Pelanggan',
+        //    'password' => Hash::make('12345'),
+        //    'level_id' => 4
+        //];
 
         // Tambahkan data ke tabel m_user
-        UserModel::insert($data);
+        //UserModel::insert($data);
 
         // Ambil semua data dari tabel m_user
-        $user = UserModel::all();
+        //$user = UserModel::all();
 
         // Kirim data ke view 'user'
+        //return view('user', ['data' => $user]);
+
+        // tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+
+        UserModel::where('username', 'customer-1')->update($data); // update data user
+
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 }
