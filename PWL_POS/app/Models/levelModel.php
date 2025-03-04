b19f9e7aa3ed8427bffafcf3cdbe69d6b7ac4c5b
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2025-03-04 21:01:12 +0700

    2.7 1-3

---
+++ PWL_POS/app/Models/levelModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class levelModel extends Model
+{
+    protected $table = 'm_level';  // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'm_level';  // Mendefinisikan primary key dari tabel yang digunakan
+}
