2c191bbe04a7ddf23e09f07bb157b8ff9c72b9e5
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2025-03-05 04:10:21 +0700

    2.7 1-3

--- PWL_POS/app/Models/levelModel.php
+++ PWL_POS/app/Models/levelModel.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class levelModel extends Model
 {
     protected $table = 'm_level';  // Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'm_level';  // Mendefinisikan primary key dari tabel yang digunakan
+    protected $primaryKey = 'level_id';  // Mendefinisikan primary key dari tabel yang digunakan
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(UserModel::class, 'level_id', 'level_id');
+    }
 }
