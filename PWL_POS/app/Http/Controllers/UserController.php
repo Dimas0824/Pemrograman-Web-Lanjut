946b0a4367d2bc8eff8fdffb9c198761af1ce9fd
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2025-03-04 08:25:44 +0700

    Praktikum 1(1-3)

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -29,10 +30,14 @@ class UserController extends Controller
 
         // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_dua',
+            'nama' => 'Manager 2',
+            'password' => Hash::make('12345')
         ];
+        UserModel::create($data);
 
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        //UserModel::where('username', 'customer-1')->update($data); // update data user
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
